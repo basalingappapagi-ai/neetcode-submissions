1a48c3b40cc7c494f3a6ff71607a12f8049f8da0
basalingappapagi-ai <basalingappapagi@gmail.com>
2026-06-29 14:51:22 +0530

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+       int s=nums.size();
+        for(int i=0;i<s-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+            
+        }return false;
+        
+    }
+};
