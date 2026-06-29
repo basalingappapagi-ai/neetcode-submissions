e0315c073455f884b330e75f9c21cd1330231351
basalingappapagi-ai <basalingappapagi@gmail.com>
2026-06-29 15:30:58 +0530

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+
+        for(int n:nums){
+            s.insert(n);
+        }
+
+        return nums.size()!=s.size();
+        
+    }
+};
